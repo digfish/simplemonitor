626c6fd17f39a8ceae071dd3239daeacbd8f0457
digfish <pescadordigital@gmail.com>
2016-08-16 01:49:39 +0100

    Adjusted to work on my nas

--- Monitors/network.py
+++ Monitors/network.py
@@ -234,7 +234,7 @@ class MonitorHost(Monitor):
         elif platform.startswith('linux'):
             self.ping_command = "ping -c1 -W" + ping_ttl + " %s 2> /dev/null"
             self.ping_regexp = "bytes from"
-            self.time_regexp = "min/avg/max/.+?dev = [\d.]+/(?P<ms>[\d.]+)"
+            self.time_regexp = "min/avg/max = [\d.]+/(?P<ms>[\d.]+)"
         else: # rtt min/avg/max/mdev = 27.034/27.034/27.034/0.000 ms
             RuntimeError("Don't know how to run ping on this platform, help!")
 
